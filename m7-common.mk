a948e0f8759cd8e25297de149a6262dac3c032b1
David Ferguson <ferguson.david@gmail.com>
2013-05-29 08:02:46 -0400

    m7-common: copy nfcee_access.xml during build
    Change-Id: I8b2ac9cdca6dda9fd397d9fbb872cefe9edb9009

--- m7-common.mk
+++ m7-common.mk
@@ -118,6 +118,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := device/htc/m7-common/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := device/htc/m7-common/configs/nfcee_access_debug.xml
+endif
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp,adb
 
