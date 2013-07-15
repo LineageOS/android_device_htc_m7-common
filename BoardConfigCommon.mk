b10ca12d8f318193bea50cc56cb95ab262832d81
David Hays <dhays90@gmail.com>
2013-07-15 18:46:43 -0500

    m7-common: recovery: fix up recovery building prebuilt kernel for new touch firmware
    Change-Id: I40af5ff5c0e487ea1beb1e561d96b3e0447e4586

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,9 +96,11 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Custom Recovery
 ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
 TARGET_RECOVERY_FSTAB := device/htc/m7-common/recovery/recovery.fstab.gsm
+TARGET_RECOVERY_KERNEL := device/htc/m7-common/recovery/kernel
 else
 TARGET_RECOVERY_FSTAB := device/htc/m7-common/recovery/recovery.fstab.cdma
 endif
+TARGET_RECOVERY_INITRC := device/htc/m7-common/recovery/root/init.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/htc/m7-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
