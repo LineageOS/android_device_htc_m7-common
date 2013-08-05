0a57d758cc04c57a7bd839efbfbd3955889e9d14
kristofpetho <kristof.petho@gmail.com>
2013-08-05 22:37:52 +0200

    m7: selinux support
    Change-Id: I087f0fa797cabeeefe71be1330aae99633353a28

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,6 +91,41 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1946156032
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 27917287424
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+# SElinux
+BOARD_SEPOLICY_DIRS := \
+	device/htc/m7-common/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+	file_contexts \
+	property_contexts \
+	te_macros \
+	bluetooth_loader.te \
+	bridge.te \
+	camera.te \
+	conn_init.te \
+	device.te \
+	dhcp.te \
+	domain.te \
+	drmserver.te \
+	file.te \
+	kickstart.te \
+	init.te \
+	mediaserver.te \
+	mpdecision.te \
+	netmgrd.te \
+	property.te \
+	qmux.te \
+	restorecon.te \
+	rild.te \
+	rmt.te \
+	sensors.te \
+	surfaceflinger.te \
+	system.te \
+	tee.te \
+	thermald.te \
+	ueventd.te \
+	wpa_supplicant.te
+
 # Custom Recovery
 ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
 TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.gsm
