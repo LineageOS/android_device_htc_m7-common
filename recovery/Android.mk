4ea34ed4014849c539727414b220e1305671d6ba
Ethan Chen <intervigil@gmail.com>
2013-08-08 20:58:03 -0700

    m7-common: Fix USB charge mode
    * Switch to using lpm.rc
    * Update charge mode binaries
    * Add charger image resources
    
    Change-Id: I79efb86155c61d6eb25dd1d41fae2a27167dcb47

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,11 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.recovery.qcom.rc
+LOCAL_MODULE		:= lpm.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.recovery.qcom.rc
+LOCAL_SRC_FILES		:= etc/lpm.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= charger
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= sbin/charger
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -13,14 +21,6 @@ LOCAL_MODULE		:= choice_fn
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= sbin/choice_fn
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= detect_key
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= sbin/detect_key
 LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
@@ -33,9 +33,57 @@ LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= power_test
+LOCAL_MODULE		:= battery_0.png
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= sbin/power_test
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_SRC_FILES		:= res/images/charger/battery_0.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_1.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_1.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_2.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_2.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_3.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_3.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_4.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_4.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_fail.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_fail.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= battery_full.png
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= res/images/charger/battery_full.png
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
 include $(BUILD_PREBUILT)
