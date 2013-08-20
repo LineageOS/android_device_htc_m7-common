08f7e18f090ed58a9ebf0c6086b67269b84c6385
Randall Rushing <randall.rushing@gmail.com>
2013-08-20 13:26:50 -0700

    m7-common: HTC IR Blaster support for AOSP
    Change-Id: I0a6bd8155b9f1a3421bccbeaf5f3e94d8a15fe80

---
+++ cir/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH:=$(call my-dir)
+ifneq ($(filter m7att m7spr m7tmo m7ul,$(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := htcirlibs
+LOCAL_SRC_FILES := framework/htcirlibs.jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CIRModule
+LOCAL_SRC_FILES := app/CIRModule.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= libhtcirinterface_jni
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= SHARED_LIBRARIES
+LOCAL_SRC_FILES		:= lib/libhtcirinterface_jni.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_SHARED_LIBRARIES)
+include $(BUILD_PREBUILT)
+
+endif
