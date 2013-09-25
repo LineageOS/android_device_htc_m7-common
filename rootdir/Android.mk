cee364fd578c04ac92d6afd45c588c8c87f74648
jackmu95 <jacob.mueller.elz@gmail.com>
2013-09-25 22:47:41 +0200

    m7-common: Fix the build
    Change-Id: I5dd0fddc714119cf096de581879efa80ade7967f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,40 +25,19 @@ LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-endif
+else  # Non-GSM variants
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.qcom
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+
 ifneq ($(filter m7spr,$(TARGET_DEVICE)),)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.qcom
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom.cdma
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.cdma
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= remount.qcom
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/remount.qcom.cdma
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-else  # Verizon variant
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.qcom
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
+else
 LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
+endif
+
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -66,7 +45,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
+
+ifneq ($(filter m7spr,$(TARGET_DEVICE)),)
+LOCAL_SRC_FILES		:= etc/init.target.rc.cdma
+else
 LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
+endif
+
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -74,7 +59,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= remount.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
+
+ifneq ($(filter m7spr,$(TARGET_DEVICE)),)
+LOCAL_SRC_FILES		:= etc/remount.qcom.cdma
+else
 LOCAL_SRC_FILES		:= etc/remount.qcom.gsm
+endif
+
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif
