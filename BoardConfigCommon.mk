cd5e2a8d2120e2f9dc31bf207d8906ab9d3fc475
Nathan Grebowiec <njgreb@gmail.com>
2013-10-14 16:08:56 -0500

    Enable swipe controls in recovery
    Change-Id: I99451f13d41b85e1b3022eb039f42647261ae92b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,6 +141,7 @@ BOARD_CUSTOM_GRAPHICS := ../../../device/htc/m7-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERY_SWIPE := true
 
 # Charge mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
