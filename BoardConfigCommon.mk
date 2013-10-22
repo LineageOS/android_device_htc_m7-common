6b555b1370071a1c03744914aac6fe42855670c2
Ethan Chen <intervigil@gmail.com>
2013-10-22 01:36:11 -0700

    m7-common: remove deprecated custom recovery graphics
    Change-Id: Ib29bcf571a6c572bc480cc3cc19daa522004329b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,7 +134,6 @@ TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.spr
 else
 TARGET_RECOVERY_FSTAB := device/htc/m7-common/rootdir/etc/fstab.qcom.gsm
 endif
-BOARD_CUSTOM_GRAPHICS := ../../../device/htc/m7-common/recovery/graphics.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
