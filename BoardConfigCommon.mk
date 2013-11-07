0d7cd03835c2884f6fbe5be012e30dd8c39a3a2a
Ethan Chen <intervigil@gmail.com>
2013-11-06 23:58:41 -0800

    m7-common: use permissive SELinux
    * Switch SELinux to permissive mode until actual policies are created.
    
    Change-Id: I9bc8dbe54bd99cd11014964f6ae43c6114554963

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,7 +32,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/m7-common/include
 # Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG := cyanogenmod_m7_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/m7
