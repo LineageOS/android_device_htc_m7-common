67181072423c71eaf07658963dbf2447d8ca512b
Steve Kondik <shade@chemlab.org>
2013-12-25 18:46:22 -0800

    m7-common: Remove modem links, generate firmware symlinks
    * Because remounting the system partition to symlink firmware is dumb.
    
    Change-Id: I55b0ad232fb5dbb1abc57bcb2ace66be1737e7ca

--- Android.mk
+++ Android.mk
@@ -19,4 +19,40 @@ ifneq ($(filter m7att m7spr m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+# Create firmware links
+FIRMWARE_MDM_IMAGES := \
+    acdb.mbn \
+    apps.mbn \
+    dsp1.mbn dsp2.mbn dsp3.mbn \
+    efs1.mbn efs2.mbn efs3.mbn \
+    htc61.mbn htc62.mbn htc63.mbn htc64.mbn htc65.mbn \
+    htccdma.mbn htcnvbak.mbn htcrcust.mbn htcrfnv.mbn htcsmem.mbn htcssmem.mbn \
+    mdm_acdb.img \
+    rpm.mbn \
+    sbl1.mbn sbl1_82.mbn sbl1_92.mbn sbl1_96.mbn \
+    sbl2.mbn
+
+FIRMWARE_MDM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_MDM_IMAGES)))
+$(FIRMWARE_MDM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "MDM Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/mdm/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MDM_SYMLINKS)
+
+FIRMWARE_Q6_IMAGES := \
+    q6.b00 q6.b01 q6.b03 q6.b04 q6.b05 q6.b06 q6.mdt
+
+FIRMWARE_Q6_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_Q6_IMAGES)))
+$(FIRMWARE_Q6_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Q6 Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/q6/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_Q6_SYMLINKS)
+
 endif
