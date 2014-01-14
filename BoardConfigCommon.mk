2afafc866af8d310b81edacba201e3c0d74f2992
Ethan Chen <intervigil@gmail.com>
2014-01-13 18:08:57 -0800

    m7-common: use new common kernel
    Change-Id: Ia7367daf459ca67de1dcf9b2ba64842ba9ee6758

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,8 +34,8 @@ BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
-TARGET_KERNEL_CONFIG := cyanogenmod_m7_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/m7
+TARGET_KERNEL_CONFIG := m7_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
