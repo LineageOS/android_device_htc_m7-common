52c5417b22767b0daa59797458d17a103e887f5d
Dorian Snyder <dastin1015@gmail.com>
2014-05-07 00:18:35 -0700

    WIP: m7: consolidate all m7 variants
    Change-Id: Idcf96dbccdd55823e99afa63c674fcdfd0594147

---
+++ cm.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/htc/m7lte/full_m7lte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_m7lte
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -8,35 +8,13 @@ LOCAL_SRC_FILES		:= etc/fstab.qcom
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-ifneq ($(filter m7 m7att m7tmo m7ul,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.gsm
+LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
-
-ifeq ($(TARGET_DEVICE),m7vzw)
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-endif
-
-ifeq ($(TARGET_DEVICE),m7spr)
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.spr
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
