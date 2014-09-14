1673920cf7eefb9f19f4c3f306817b5a86348bc2
David Hays <dhays90@gmail.com>
2014-09-14 16:23:05 -0500

    m7-common: HTC M7 stock offmode charging images
    *1% battery increments
    
    Change-Id: Ic87b2307e88636ffdf0d971c49bed87faf7aa378

--- m7-common.mk
+++ m7-common.mk
@@ -36,20 +36,13 @@ PRODUCT_PACKAGES += \
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 PRODUCT_PACKAGES += \
     lpm.rc \
-    charger \
     choice_fn \
     offmode_charging \
     chargeled \
     init.recovery.qcom.rc
 
-PRODUCT_PACKAGES += \
-    battery_0.png \
-    battery_1.png \
-    battery_2.png \
-    battery_3.png \
-    battery_4.png \
-    battery_fail.png \
-    battery_full.png
+# Recovery offmode charging images
+$(call inherit-product, device/htc/m7-common/recovery/res/offmode_charging_images.mk)
 
 # QC thermald config
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
--- recovery/Android.mk
+++ recovery/Android.mk
@@ -9,14 +9,6 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= charger
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= sbin/charger
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= choice_fn
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
@@ -30,60 +22,4 @@ LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= sbin/offmode_charging
 LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_0.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_0.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_1.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_1.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_2.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_2.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_3.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_3.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_4.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_4.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_fail.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_fail.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= battery_full.png
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= res/images/charger/battery_full.png
-LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)/res/images/charger
 include $(BUILD_PREBUILT)
