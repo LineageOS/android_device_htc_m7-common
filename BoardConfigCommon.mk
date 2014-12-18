029c50f4d33ca42edc86fc8f86a279cf9c7187ca
Ethan Chen <intervigil@gmail.com>
2014-12-17 18:48:15 -0800

    Revert "Revert "Revert "m7-common: use permissive SELinux"""
    * Run in enforcing mode
    
    This reverts commit 86322cf314651e46457846f725d7b2007ddbe17b.
    
    Change-Id: I07b62823f1667e8bf1a3bff9ee9a2c2bdb02a22e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 # Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := m7_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
