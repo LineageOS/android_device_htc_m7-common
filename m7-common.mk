c3817b8b074667755a6424b0205c0f7ad5790c52
Ethan Chen <intervigil@gmail.com>
2015-01-11 14:24:05 -0800

    m7-common: Remove deprecated torch setup
    Change-Id: I4873ad3faafee1a65dd5e8606bd2aec6b5ca35a7

--- m7-common.mk
+++ m7-common.mk
@@ -130,10 +130,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
 
-# Torch
-PRODUCT_PACKAGES += \
-    Torch
-
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface
