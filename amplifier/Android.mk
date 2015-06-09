14b010f917d6e9b12d5aeeebc639a59db3708eb4
Scott Mertz <scott@cyngn.com>
2015-06-08 18:51:54 -0700

    m7-common: Refactor TFA9887 amplifier code
    * Use struct to contain amp device state
    * Clean up variable naming
    * Standardize file load functions
    * Standardize amplifier device array
    * Use amplifier register names for startup
    * Initialize amp on open
    * Open I2S interface on initialization
    * Close amplifier descriptors on close
    
    Change-Id: I2e5366b4389cd6d001d83b64214cd136304522b1

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -3,9 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := \
-	liblog libutils
+	liblog libutils libtinyalsa
 
 LOCAL_C_INCLUDES := \
+	external/tinyalsa/include \
 	hardware/libhardware/include
 
 LOCAL_SRC_FILES := \
