92c7ceaf472513f547bbe554fc2847c592c204ea
David Hays <dhays90@gmail.com>
2015-10-12 14:42:14 -0500

    m7-common: Remove deprecated USB OTG property
    Reverts 7b19a0bd8a9fa5736076108d55efebe6e840e2ca
    
    Change-Id: Iaa48377ed6d6d879eb5ebe00c89df0c3bda47f88

--- system_prop.mk
+++ system_prop.mk
@@ -33,7 +33,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
-
-# USB OTG interface
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.isUsbOtgEnabled=true
