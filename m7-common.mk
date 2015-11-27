03658184ae726b8957d00f20212141a252324fc6
Ethan Chen <intervigil@gmail.com>
2015-11-27 17:22:00 -0600

    m7-common: Shim libhtc_symbols instead
    Change-Id: I4e417b07b2c1749e4831b8a611f98b335c14ea33

--- m7-common.mk
+++ m7-common.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
-# HTC symbols
-PRODUCT_PACKAGES += \
-    libhtc_symbols
-
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/projector_input.idc:system/usr/idc/projector_input.idc \
@@ -97,6 +93,10 @@ PRODUCT_COPY_FILES += \
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8960
+
+# Log
+PRODUCT_PACKAGES += \
+    liblog_shim
 
 # Media
 PRODUCT_COPY_FILES += \
