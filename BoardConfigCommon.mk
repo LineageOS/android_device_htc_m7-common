e73bcb10cd76145793eab40d29b8742e513d4a57
David Hays <dhays90@gmail.com>
2016-01-11 12:09:34 -0600

    m7-common: Build kernel with gcc 4.9
    Change-Id: I77641b49f8a078b52c61d8acc4d6306321fe5bf3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,6 +37,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := m7_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Audio
