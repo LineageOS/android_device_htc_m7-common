87fe60250e7a447d042f19685b75e58f2c88d084
Ethan Chen <intervigil@gmail.com>
2016-05-06 00:14:00 -0500

    m7-common: Clean up Android.mk
    Change-Id: I1a9021193b420bdc94a0e3b5cb9e226519d5c283

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter m7 m7att m7spr m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
