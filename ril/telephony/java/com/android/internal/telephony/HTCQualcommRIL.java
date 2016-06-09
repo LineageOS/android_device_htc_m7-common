d1e082aafa93bfad0f4d7f07eb4d6b20794553d5
Matt Mower <mowerm@gmail.com>
2016-06-09 02:22:32 -0500

    m7-common: HTCQualcommRIL: Handle invalid ICC card states
    Change-Id: Ied442e39d0fee0a23b4bd740bc03d1d9bd1304ac

--- ril/telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -58,6 +58,24 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
         super(context, networkMode, cdmaSubscription, instanceId);
     }
 
+    @Override
+    protected Object
+    responseIccCardStatus(Parcel p) {
+        final int CARDSTATE_ABSENT = 0;
+        final int CARDSTATE_ERROR = 2;
+        int dataPosition = p.dataPosition();
+        int cardState = p.readInt();
+
+        if (cardState > CARDSTATE_ERROR) {
+            p.setDataPosition(dataPosition);
+            p.writeInt(CARDSTATE_ABSENT);
+        }
+
+        p.setDataPosition(dataPosition);
+
+        return super.responseIccCardStatus(p);
+    }
+
     private static String
     responseToStringHTC(int request) {
         switch(request) {
