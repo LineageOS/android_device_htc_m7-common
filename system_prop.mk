1ff9ea502e60de2a57339d9f91261eba578737f3
Matt Mower <mowerm@gmail.com>
2016-06-21 20:51:15 -0500

    m7-common: Move legacy audio flags/props down from msm8960-common
    Change-Id: I772f1270b1f1c12aee76efb71a8a33db068de679

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,15 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    persist.audio.vr.enable=false \
+    qcom.hw.aac.encoder=true \
+    ro.qc.sdk.audio.fluencetype=fluence
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bt.bdaddr_path=/sys/module/htc_bdaddress/parameters/bdaddress
