2b5d15a6218099c6a2bd79cc0756e6cdb5d132b7
Matt Mower <mowerm@gmail.com>
2016-07-21 14:43:30 -0500

    m7-common: Use common extract-utils
    Change-Id: I5eca857f0e5eca85e7c876af3965f733a5b08e95

--- extract-files.sh
+++ extract-files.sh
@@ -1,67 +1,59 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
-  fi
 fi
+. "$HELPER"
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-if [ -f ../$DEVICE/device-proprietary-files.txt ]; then
-  for FILE in `egrep -v '(^#|^$)' ../$DEVICE/device-proprietary-files.txt`; do
-    echo "Extracting /system/$FILE ..."
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-      mkdir -p $BASE/$DIR
-    fi
-    if [ "$SRC" = "adb" ]; then
-      adb pull /system/$FILE $BASE/$FILE
+if [ $# -eq 0 ]; then
+    SRC=adb
+else
+    if [ $# -eq 1 ]; then
+        SRC=$1
     else
-      cp $SRC/system/$FILE $BASE/$FILE
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
     fi
-  done
 fi
 
-for FILE in `egrep -v '(^#|^$)' ../m7-common/proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
-done
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
 
-BASE=../../../vendor/$VENDOR/m7-common/proprietary
-rm -rf $BASE/*
-for FILE in `egrep -v '(^#|^$)' ../m7-common/common-proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
-done
+extract "$MY_DIR"/common-proprietary-files.txt "$SRC"
 
-./../m7-common/setup-makefiles.sh
+# Reinitialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
+extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
