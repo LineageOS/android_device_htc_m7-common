d942184d5f5e16c15ffa91fd022ea364c14424e7
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-24 16:58:25 -0400

    bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)
    * Kernel is still using drivers/rtc/alarm.c, instead of the newer
      kernel/time/alarmtimer.c
    
    Change-Id: I28531827c0937408f08479bf03087566a5b323ef

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,7 @@
 #define BTM_DEF_LOCAL_NAME   "HTC One"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define I2SPCM_SLAVE_BRCM TRUE
+
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
