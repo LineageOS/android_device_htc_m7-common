5be8d5c49757d071075c3c0ba458efd3ab687ee6
burchnb <nolanjburchfield@gmail.com>
2016-11-04 12:36:09 -0500

    responseIccCardStatus changes
    * Fix for *86 and +1 calls on htc msm8960 vzw devices
    
    Change-Id: I594cf48b081ecf74bc861e6e1f8d22962fb367cc

--- ril/telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -61,19 +61,38 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
     @Override
     protected Object
     responseIccCardStatus(Parcel p) {
-        final int CARDSTATE_ABSENT = 0;
-        final int CARDSTATE_ERROR = 2;
-        int dataPosition = p.dataPosition();
-        int cardState = p.readInt();
-
-        if (cardState > CARDSTATE_ERROR) {
-            p.setDataPosition(dataPosition);
-            p.writeInt(CARDSTATE_ABSENT);
+        IccCardApplicationStatus appStatus;
+        IccCardStatus cardStatus = new IccCardStatus();
+        cardStatus.setCardState(p.readInt());
+        cardStatus.setUniversalPinState(p.readInt());
+        cardStatus.mGsmUmtsSubscriptionAppIndex = p.readInt();
+        cardStatus.mCdmaSubscriptionAppIndex = p.readInt();
+        cardStatus.mImsSubscriptionAppIndex = p.readInt();
+        int numApplications = p.readInt();
+        // limit to maximum allowed applications
+        if (numApplications > IccCardStatus.CARD_MAX_APPS) {
+            numApplications = IccCardStatus.CARD_MAX_APPS;
         }
-
-        p.setDataPosition(dataPosition);
-
-        return super.responseIccCardStatus(p);
+        cardStatus.mApplications = new IccCardApplicationStatus[numApplications];
+        for (int i = 0 ; i < numApplications ; i++) {
+            appStatus = new IccCardApplicationStatus();
+            appStatus.app_type       = appStatus.AppTypeFromRILInt(p.readInt());
+            appStatus.app_state      = appStatus.AppStateFromRILInt(p.readInt());
+            appStatus.perso_substate = appStatus.PersoSubstateFromRILInt(p.readInt());
+            appStatus.aid            = p.readString();
+            appStatus.app_label      = p.readString();
+            appStatus.pin1_replaced  = p.readInt();
+            appStatus.pin1           = appStatus.PinStateFromRILInt(p.readInt());
+            appStatus.pin2           = appStatus.PinStateFromRILInt(p.readInt());
+            // All subsequent readInt()s added for our device
+            p.readInt(); // pin1_num_retries
+            p.readInt(); // puk1_num_retries
+            p.readInt(); // pin2_num_retries
+            p.readInt(); // puk2_num_retries
+            p.readInt(); // perso_unblock_retries
+            cardStatus.mApplications[i] = appStatus;
+        }
+        return cardStatus;
     }
 
     private static String
