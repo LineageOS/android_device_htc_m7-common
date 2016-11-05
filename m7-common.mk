67a05b5da01cf82de3898bf488c904e6af82e30a
Arne Coucheron <arco68@gmail.com>
2016-11-05 19:13:10 +0200

    m7-common: Add shim for libwvm.so
    * Update libshims to c++
    
    Change-Id: I7bc7b8cbc571dcc4dac4fb5078988733fea5d3a2

--- m7-common.mk
+++ m7-common.mk
@@ -136,6 +136,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
+# Widevine
+PRODUCT_PACKAGES += \
+    libshim_wvm
+
 # WiFi
 PRODUCT_PACKAGES += \
     libnetcmdiface
