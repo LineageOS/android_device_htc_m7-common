fced1eb999cd4b146ff72e0c2f072e75033c0adf
Matt Mower <mowerm@gmail.com>
2017-01-07 03:18:34 -0600

    m7-common: Workaround failure to load libpn544_fw
    Android 7 introduced additional restrictions on which libraries apps
    are allowed to load. They either need to be housed with the
    application or found in one of the standard library paths (/system/lib
    or /system/vendor/lib):
    
    https://developer.android.com/about/versions/nougat/android-7.0-changes.html#ndk
    
    Rather than updating the Nfc package to point to a new firmware
    library location, we can simply relocate the library to an allowed
    path and drop-in a symlink at the old location. This seems to make
    everything work as expected.
    
    Error for reference:
    
    E linker  : library "/vendor/firmware/libpn544_fw.so" ("/system/vendor/firmware/libpn544_fw.so") needed or dlopened by "/system/lib/libnfc.so" is not accessible for the namespace: [name="classloader-namespace", ld_library_paths="", default_library_paths="/system/app/Nfc/lib/arm:/system/app/Nfc/Nfc.apk!/lib/armeabi-v7a:/system/lib:/system/vendor/lib", permitted_paths="/data:/mnt/expand:/data/data/com.android.nfc:/system/lib:/system/vendor/lib"]
    E NFC-HCI : Could not open /vendor/firmware/libpn544_fw.so
    
    Change-Id: Iac95b4abb1d234736c00f81c4669e28a335e8750

--- nfc/Android.mk
+++ nfc/Android.mk
@@ -19,9 +19,15 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libpn544_fw.c
 LOCAL_MODULE := libpn544_fw
 LOCAL_MODULE_OWNER := nxp
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/firmware
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACK_MODULE_RELOCATIONS := false
+
+# Symlink /vendor/firmware/libpn544_fw.so to /vendor/lib/firmware/libpn544_fw.so
+LOCAL_POST_INSTALL_CMD := \
+    $(hide) mkdir -p $(TARGET_OUT_VENDOR)/firmware && \
+    rm -f $(TARGET_OUT_VENDOR)/firmware/libpn544_fw.so && \
+    ln -sf /system/vendor/lib/firmware/libpn544_fw.so $(TARGET_OUT_VENDOR)/firmware/libpn544_fw.so
 
 include $(BUILD_SHARED_LIBRARY)
 
