ca012ae53bc8ff2c59e7a96d16f9a5f360f10b43
Gabriele M <moto.falcon.git@gmail.com>
2017-01-30 21:52:36 -0600

    m7-common: Fix vendor/qcom blob extraction/makefiles
    Squash of two commits, where msm8974 has been replaced with msm8960
    for this device.
    
    * msm8226-common: Generate graphics vendor Makefile if missing
    
      This allows to build CM without cloning vendor repos.
    
      Change-Id: I87777485edc5f29c5899091176e55d28e36bb263
    
    * msm8226-common: Fix blobs extraction with no vendor repos
    
      Commit bff1cb1def84 ("msm8226-common: Generate graphics vendor
      Makefile if missing") fixed only the generation of the makefiles,
      but it didn't take care of the actual extraction of the blobs.
    
      Change-Id: Idd38fbff554c0dac4b7cb8a867abb017bf37afc3
    
    Change-Id: Ic9fd0779dd3decd7cda7c4e17bf449d6991dd70b

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,7 +39,11 @@ write_headers "m7 m7spr m7vzw"
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ -s "$CM_ROOT"/vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    write_makefiles "$MY_DIR"/../../qcom/common/extractors/graphics-msm8960.txt
+fi
 
 write_footers
 
